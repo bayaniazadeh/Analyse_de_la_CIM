fd572e9acd16dee32c388ea681aa334497e05ef5
bayaniazadeh <bayaniazadeh@gmail.com>
2023-08-21 21:16:57 +0330

    Add files via upload
    Analyse du modèle de régression

---
+++ Icd11_eliX.R
@@ -0,0 +1,40 @@
+#install.packages("writexl")
+library(stats)
+library(MASS)
+library(writexl)
+
+data <- read.csv("/Users/azadehbayani/Documents/Azadé/UdeM/Master/Projet/Regression/code/R_data.csv")
+
+full.model <- glm(death ~ Chf + Carit+Valv+Pcd+Pvd+Hypunc+Hypc+Para+
+Ond+Cpd+Diabunc+Diabc+Hypohthy+Rf+Ld+Pud+Aids+
+Lymph+Metacanc+Solidtum+Rheumd+Coag+Obes+Wloss+Fed+Blane+
+Dane+Alcohol+Drug+Psycho+Depre, family = binomial(link = "logit"), data = data)
+
+summary(full.model)
+backward.model <- stepAIC(full.model, direction = "backward")
+
+coef <- coef(backward.model)
+
+summary(backward.model)
+p_values <- summary(backward.model)$coefficients[, "Pr(>|z|)"]
+
+# Print the p-values
+print(p_values)
+
+backward.model$
+print(coef)
+min_c = min(abs(coef))
+weighted_coeffs = abs(coef)/min_c
+print(weighted_coeffs)
+
+
+# Create a new data frame to store the summary statistics and coefficients
+result_df <- data.frame(
+  #Summary = c("Coefficients", summary(backward.model)),
+  Coefficients = coef,
+  P_Values = p_values,
+  weighted_coeffs = weighted_coeffs
+)
+
+
+
